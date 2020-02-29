25ced68ea752ad1f466b43266b627953b8562906
Daniel <garciabusiness21@gmail.com>
2020-02-28 23:36:18 -0600

    facebook authentication, button not showing

--- authenticate.js
+++ authenticate.js
@@ -2,22 +2,23 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;  //provides several helper methods
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const ExtractJwt = require('passport-jwt').ExtractJwt;  
+const jwt = require('jsonwebtoken'); 
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 
-exports.local = passport.use(new LocalStrategy(User.authenticate()));   //passport.use adds specific Strategy plugin, LocalStrategy takes 
-                                                                        //callback function to verify username and password against locally stored credentials
+exports.local = passport.use(new LocalStrategy(User.authenticate()));   
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600}); //sign method takes user object that was passed in as first arg, second arg is secret key string from config.js
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600}); 
 };
 
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); //specifies how JSON web token should be extracted from incoming request message
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); 
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(
@@ -38,7 +39,7 @@ exports.jwtPassport = passport.use(
     )
 );
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});    //verify incoming request is coming from verified user
+exports.verifyUser = passport.authenticate('jwt', {session: false});    
 
 exports.verifyAdmin = function(req, res, next){
     if (req.user.admin === true){
@@ -49,3 +50,34 @@ exports.verifyAdmin = function(req, res, next){
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
